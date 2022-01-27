c5d3b311be0b851857e620c5f96ff2f2ebce8b59
Admin <Admin@DESKTOP-NTSGNBR>
2022-01-27 20:02:19 +0330

    Initial Repository

---
+++ MozdAsa.Core/Services/CompanyService.cs
@@ -0,0 +1,29 @@
+﻿using MozdAsa.Core.Interfaces;
+using MozdAsa.Core.ViewModels;
+using MozdAsa.Domain.Entity;
+using MozdAsa.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MozdAsa.Core.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private ICompanyRepository _companyRepository;
+        public CompanyService(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        public CompanyViewModel GetCompany()
+        {
+            return new CompanyViewModel()
+            {
+                companies = _companyRepository.GetCompanies()
+            };
+        }
+    }
+}
---
+++ MozdAsa.Data/Repository/CompanyRepository.cs
@@ -0,0 +1,24 @@
+﻿using MozdAsa.Data.Context;
+using MozdAsa.Domain.Entity;
+using MozdAsa.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MozdAsa.Data.Repository
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private MozdAsaDbContext _ctx;
+        public CompanyRepository(MozdAsaDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Company> GetCompanies()
+        {
+            return _ctx.Companies;
+        }
+    }
+}
