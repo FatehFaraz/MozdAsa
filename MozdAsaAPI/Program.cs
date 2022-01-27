32e1028845e3f3cec75eb2076ca05dc1561e16a8
Admin <Admin@DESKTOP-NTSGNBR>
2022-01-27 22:55:43 +0330

    Update Web API

--- MozdAsaAPI/Program.cs
+++ MozdAsaAPI/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using MozdAsa.Data.Context;
+using MozdAsa.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 ConfigurationManager configuration = builder.Configuration;
+var container = new ServiceCollection();
+
 builder.Services.AddDbContext<MozdAsaDbContext>(options => 
     options.UseSqlServer(configuration.GetConnectionString("MozdAsaDbConection")));
+//RegisterServices(services);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -28,3 +32,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
