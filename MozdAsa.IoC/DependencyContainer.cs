d00b9d38ef94d87bb6d66049919fb74f4ca29044
Admin <Admin@DESKTOP-NTSGNBR>
2022-01-27 20:20:11 +0330

    Initial IoC Layer

---
+++ MozdAsa.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MozdAsa.Core.Interfaces;
+using MozdAsa.Core.Services;
+using MozdAsa.Data.Repository;
+using MozdAsa.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MozdAsa.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddScoped<ICompanyService, CompanyService>();
+            service.AddScoped<ICompanyRepository, CompanyRepository>();
+        }
+    }
+}
