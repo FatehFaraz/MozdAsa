9fe09482b0e33fe3be9bb744241821d5dc12f701
Admin <Admin@DESKTOP-NTSGNBR>
2022-01-28 07:03:55 +0330

    Update API

---
+++ MozdAsaWebAPI/Controllers/CompanyController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MozdAsa.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MozdAsaWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyController : ControllerBase
+    {
+        private ICompanyService _companyService;
+
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        private readonly ILogger<CompanyController> _logger;
+
+        public CompanyController(ILogger<CompanyController> logger, ICompanyService companyService)
+        {
+            _logger = logger;
+            _companyService = companyService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            var result = new ObjectResult(_companyService.GetCompany())
+            {
+                StatusCode = (int)HttpStatusCode.OK
+            };
+ 
+            return result;
+           
+        }
+    }
+}
